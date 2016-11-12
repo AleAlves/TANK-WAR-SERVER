6b27cec473b8dd9c79b7cef0f4abd538b75c839e
AleAlves <aleson.a.s@gmail.com>
2016-11-12 02:41:22 -0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log("Server on");
+
+var express = require('express');
+var beep = require('beepbeep');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = 3000;
+io.set('heartbeat interval', 1);
+io.set('heartbeat timeout', 240000);
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+var client = new Array(0);
+var acessos = 0;
+
+io.sockets.on('connection', function (socket) {
+    if(client.length < 2 ){
+
+      socket.emit('id',client.length%2);
+
+      client[client.length%2] = socket;
+
+      if(client.length == 2){
+            for(var i = 0; i < client.length;i++){
+                client[i].emit('gameOn',1);
+            }
+            console.log('Game on');
+        }
+
+      console.log("New connection:  " +  socket.request.connection.remoteAddress +" Player Online:  "+client.length +" Index: "+client.indexOf(socket));
+
+      socket.on('update', function(id,msg){
+
+          switch(id)
+          {
+            case 0:
+                if(client.length == 2){
+                  client[1].emit('serverData', msg);
+                }
+              break;
+            case 1:
+                if(client.length == 2){
+                  client[0].emit('serverData', msg);
+                }
+              break;
+          }
+
+    }, 0);
+
+    socket.on('disconnect', function(socket) {
+      for(var i = 0; i < client.length; i++){
+            client[i].emit("off",1);
+      }
+      for(var j = 0; j < 2; j++)
+          client.pop();
+        console.log("saiu...Paritda encerrada, sala esvaziada:"+client.length);
+        beep();
+    });
+    }
+    else{
+        console.log("sala cheia");
+        socket.emit("full",1);
+    }
+});
